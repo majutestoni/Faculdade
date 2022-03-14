c534c1b018a2438bb871f49f5e3d2084b9e81878
Maju Testoni <93037619+majutestoni@users.noreply.github.com>
2022-03-14 20:48:13 -0300

    Unidade2

---
+++ unidade2/src/Ex01.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("deu certo");
+
+        int valor1;
+        int valor2;
+        // Ex01
+        System.out.println("Leia valor 1");
+        valor1 = scan.nextInt();
+        System.out.println("Digite valor 2");
+        valor2 = scan.nextInt();
+
+        System.out.println(valor1 + valor2);
+
+        // Ex02
+        System.out.println("Valor3");
+        double valor3 = scan.nextDouble();
+        System.out.println("Valor4");
+        double valor4 = scan.nextDouble();
+
+        System.out.println(valor3 * valor4);
+
+        //Ex03
+        System
+    }
+
+}
