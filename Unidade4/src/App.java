359f3b467938a49c89b3b934067e7de5a26d4928
Maju Testoni <majuliatestoni@gmail.com>
2022-04-08 13:12:11 -0300

    ex 26

--- Unidade4/src/App.java
+++ Unidade4/src/App.java
@@ -5,31 +5,7 @@ public class App {
         Scanner scan = new Scanner(System.in);
         System.out.println("Teste"); // ambiente para testar cada questão separadamente
 
-        // Ex26 -> utilizando caracter
-        System.out.println("Escolha uma opção: T -> triangulo, Q -> quadrado, R -> Retangulo, C -> Circulo");
-        char opcaoAreas = scan.nextLine().charAt(0);
 
-        switch (opcaoAreas) {
-            case 'T':
-                System.out.println("Digite a base e a altura: ");
-                int base = scan.next
-                break;
-        switch (opcaoAreas) {
-            case 'Q':
-                
-                break;
-        switch (opcaoAreas) {
-            case 'R':
-                
-                break;
-        switch (opcaoAreas) {
-            case 'C':
-                
-                break;
-        
-            default:
-                break;
-        }
 
         scan.close();
     }
