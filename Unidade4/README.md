3481310d55edf6e46072fa205e8db206a6f88d2b
Maju Testoni <majuliatestoni@gmail.com>
2022-04-13 13:10:34 -0300

    a

--- Unidade4/README.md
+++ Unidade4/README.md
@@ -21,47 +21,4 @@ Exercicios das aulas sobre a Unidade 4 -> Referente a condições de desvios
     !
 }
 
-        // 27 -> Pedagio estacionamento
-        System.out.println("Digite a hora de chegada");
-        int horaChegada = scan.nextInt();
-        System.out.println("Digite a minuto de chegada");
-        int minutoChegada = scan.nextInt();
-        System.out.println("Digite hora de saida");
-        int horaSaida = scan.nextInt();
-        System.out.println("Digite minuto de saida");
-        int minutoSaida = scan.nextInt();
-        int qtdHoras = 0;
 
-        if (horaChegada > horaSaida) {
-            qtdHoras = 24 - (horaChegada - horaSaida);
-        } else {
-            qtdHoras = horaSaida - horaChegada;
-        }
-        int minuto = 60 - minutoChegada + minutoSaida;
-        if (minuto >= 30) {
-            qtdHoras++;
-        }
-
-        double precoCobrado = 0;
-        switch (qtdHoras) {
-            case 0:
-                System.out.println("Não precisa pagar");
-                break;
-            case 1:
-                precoCobrado = 5.00;
-                break;
-            case 2:
-                precoCobrado = 10.00;
-                break;
-            case 3:
-                precoCobrado = 17.5;
-                break;
-            case 4:
-                precoCobrado = 25.00;
-                break;
-            default:
-                precoCobrado = 25 + (10 * (qtdHoras - 4));
-                break;
-        }
-        System.out.println("quatidade de horas " + qtdHoras);
-        System.out.println("O preço cobrado será de " + precoCobrado);
--- Unidade4/src/App.java
+++ Unidade4/src/App.java
@@ -3,9 +3,52 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Teste"); // ambiente para testar cada questão separadamente
+        System.out.println("Teste"); // ambiente para testar cada questão separadamente,
 
+        // 27 -> Pedagio estacionamento
+        System.out.println("Digite o hora de chegada");
+        int horaChegada = scan.nextInt();
+        System.out.println("Digite a minuto de chegada");
+        int minutoChegada = scan.nextInt();
+        System.out.println("Digite hora de saida");
+        int horaSaida = scan.nextInt();
+        System.out.println("Digite o minuto de saida");
+        int minutoSaida = scan.nextInt();
+        int qtdHoras = 0;
 
+        if (horaChegada > horaSaida) {
+            qtdHoras = 24 - (horaChegada - horaSaida);
+        } else {
+            qtdHoras = horaSaida - horaChegada;
+        }
+        int minuto = 60 - (minutoSaida - minutoChegada);
+        if (minuto >= 30 && minuto != 60) {
+            qtdHoras++;
+        }
+
+        double precoCobrado = 0;
+        switch (qtdHoras) {
+            case 0:
+                System.out.println("Não precisa pagar");
+                break;
+            case 1:
+                precoCobrado = 5.00;
+                break;
+            case 2:
+                precoCobrado = 10.00;
+                break;
+            case 3:
+                precoCobrado = 17.5;
+                break;
+            case 4:
+                precoCobrado = 25.00;
+                break;
+            default:
+                precoCobrado = 25 + (10 * (qtdHoras - 4));
+                break;
+        }
+        System.out.println("quatidade de horas " + qtdHoras);
+        System.out.println("O preço cobrado será de " + precoCobrado);
 
         scan.close();
     }
