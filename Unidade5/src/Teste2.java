1a671f57ffbe280bc0c7b18ba6f5f33ef9cbf3ec
Maju Testoni <majuliatestoni@gmail.com>
2022-04-30 14:05:19 -0300

    ex 04 e 06

--- Unidade5/src/Teste2.java
+++ Unidade5/src/Teste2.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
+
 public class Teste2 {
     public static void main(String[] args) {
-        System.out.println("teste");
+        Scanner scan = new Scanner(System.in);
 
+        // Ex04 -> Verificar a questão do denominador
+        double numrador = 1;
+        double denominador = 0;
+        double respostaS = 0;
+        double termos;
+        for (int i = 1; i <= 20; i++) {
+            numrador += 2;
+            denominador += 2 + denominador;
+            termos = numrador / denominador;
+            respostaS += termos;
+            System.out.println(numrador);
+            System.out.println(denominador);
+        }
+
+        System.out.println(respostaS);
+
+        scan.close();
 
     }
 }
