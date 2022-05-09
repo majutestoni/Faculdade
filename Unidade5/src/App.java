0d280d0845fb1d81defa7e73784723959b4ab1b8
Maju Testoni <majuliatestoni@gmail.com>
2022-05-09 19:58:39 -0300

    unidade 5 ex 7,8

--- Unidade5/src/App.java
+++ Unidade5/src/App.java
@@ -38,20 +38,18 @@ public class App {
         System.out.println("A soma dos termos é " + somaTermos);
 
         // Ex04 -> Verificar a questão do denominador
-        double numrador = 1;
-        double denominador = 0;
-        double respostaS = 0;
-        double termos;
+        double numerador = 3;
+        double denominador = 2;
+        double numPar = 4;
+        double termos = 0;
         for (int i = 1; i <= 20; i++) {
-            numrador += 2;
-            denominador += 2 + denominador;
-            termos = numrador / denominador;
-            respostaS += termos;
-            System.out.println(numrador);
-            System.out.println(denominador);
+            termos = numerador / denominador;
+            numerador += 2;
+            denominador = denominador + numPar;
+            numPar += 2;
         }
 
-        System.out.println(respostaS);
+        System.out.println("A resposta é:" + termos);
 
         // Ex05
         // padrao 8 10 16 18 32 64 -> n4 = n1 + n2, n3 = n4 - 2
@@ -83,5 +81,43 @@ public class App {
         System.out.println(mediaAltura);
 
         scan.close();
+
+        // Ex07
+        System.out.println("Escreva o valor de n:");
+        int n = scan.nextInt();
+        int i = 0;
+        int nMenor = 0;
+        int nMaior = 0;
+        while (i < n) {
+            System.out.println("Digite numeros: ");
+            int numerosN = scan.nextInt();
+            if (numerosN < nMenor) {
+                nMenor = numerosN;
+            }
+
+            if (numerosN > nMaior) {
+                nMaior = numerosN;
+            }
+            i++;
+        }
+        System.out.println("O numero menor é " + nMenor + " o numero maior é " + nMaior);
+
+        // Ex08
+        System.out.println("Escreva o valor de p:");
+        int p = scan.nextInt();
+        int j = 0;
+        int pMenor = 0;
+        int contadorP = 0;
+        while (j < p) {
+            System.out.println("Digite numeros: ");
+            int numerosP = scan.nextInt();
+            if (numerosP < pMenor && numerosP < 0) {
+                pMenor = numerosP;
+            }
+            contadorP = contadorP + numerosP;
+            j++;
+        }
+        int mediaP = contadorP / p;
+        System.out.println("O numero negativo é " + pMenor + " a media é " + mediaP);
     }
 }
--- Unidade5/src/Teste2.java
+++ Unidade5/src/Teste2.java
@@ -4,7 +4,23 @@ public class Teste2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Ex15 -> While
+        System.out.println("Digite o nome do aluno");
+        String nome = scan.next();
+        while (nome == "fim") {
+            System.out.println("teste");
+            System.out.println("nota1");
+            int nota1 = scan.nextInt();
+            System.out.println("nota2");
+            int nota2 = scan.nextInt();
 
+            int media = (nota1 + nota2) / 2;
+            System.out.println(media);
+
+            System.out.println("Digite o nome do aluno");
+            nome = scan.nextLine();
+
+        }
 
         scan.close();
 
