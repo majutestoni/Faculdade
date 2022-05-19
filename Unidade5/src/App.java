34e3a6b63eef0a3fc5595a906ed728958fa15953
Maju Testoni <majuliatestoni@gmail.com>
2022-05-18 21:01:36 -0300

    23

--- Unidade5/src/App.java
+++ Unidade5/src/App.java
@@ -351,6 +351,34 @@ public class App {
         }
         System.out.println("O salario finalizou com " + salarioFuncionario);
 
+        // 23
+        System.out.println("deseja digitar os dados de mais um vendedor: s (SIM) / n (NÃO)");
+        String maisUmVendedor = scan.nextLine();
+
+        String nomeFuncionarios = "";
+        int itensVendidos = 1;
+        double valorDoProduto = 0, valorTotalDeVenda = 0, salarioFuncionarioTotal = 0;
+        while (maisUmVendedor.equals("s")) {
+            System.out.println("Digite o nome do funcionario");
+            nomeFuncionarios = scan.next();
+
+            System.out.println("digite quantos itens");
+            itensVendidos = scan.nextInt();
+
+            for (int h = 1; h <= itensVendidos; h++) {
+                System.out.println("Digite o valor do produto");
+                valorDoProduto = scan.nextDouble();
+                valorTotalDeVenda = valorTotalDeVenda + valorDoProduto;
+            }
+            salarioFuncionarioTotal = 0.3 * valorTotalDeVenda;
+
+            System.out.println("O funcionario " + nomeFuncionarios + " teve um total de venda de " + valorTotalDeVenda
+                    + " de " + itensVendidos + " itens vendidos " + " e o salario de " + salarioFuncionarioTotal);
+
+            System.out.println("deseja digitar os dados de mais um vendedor: s (SIM) / n (NÃO)");
+            maisUmVendedor = scan.next();
+        }
+
         scan.close();
     }
 }
--- Unidade5/src/teste.java
+++ Unidade5/src/teste.java
@@ -4,7 +4,26 @@ public class teste {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // 24
+        System.out.println("Informe o limite de peso diario (KG)");
+        double pesoDePescaLimite = scan.nextDouble();
+        double pesoDePescaLimiteTotal = 0, pesoPeixe = 0;
+        String querContinuarAPescar = "";
 
+        while (pesoDePescaLimiteTotal <= pesoDePescaLimite) {
+            System.out.println("deseja informar o peso de mais um peixe: s (SIM) / n (NÃO)?");
+            querContinuarAPescar = scan.next();
+            while (querContinuarAPescar.equals("s")) {
+                System.out.println("Digite o peso do peixe(Em gramas)");
+                pesoPeixe = scan.nextDouble();
+
+                System.out.println(pesoDePescaLimiteTotal);
+                System.out.println("deseja informar o peso de mais um peixe: s (SIM) / n (NÃO)?");
+                querContinuarAPescar = scan.next();
+            }
+            pesoDePescaLimiteTotal = pesoDePescaLimiteTotal + pesoPeixe;
+
+        }
 
         scan.close();
     }
