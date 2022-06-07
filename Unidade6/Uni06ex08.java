6031933f9f11434efc9faa7ea297b116ae2ef836
Maju Testoni <majuliatestoni@gmail.com>
2022-06-07 19:11:44 -0300

    fazendo 8

--- Unidade6/Uni06ex08.java
+++ Unidade6/Uni06ex08.java
@@ -12,21 +12,41 @@ public class Uni06ex08 {
         }
 
         double vetor[] = new double[N];
+        int vetAuxiliar[] = new int[N];
+        double vetFinal[] = new double[N];
 
-        ler(vetor, scan);
-
+        ler(vetor, scan, vetAuxiliar);
+        mostrar(vetAuxiliar, vetor, vetFinal);
         scan.close();
     }
 
-    private void ler(double vetor[], Scanner scan) {
+    private void ler(double vetor[], Scanner scan, int vetAuxiliar[]) {
+
+        int aux = 0;
 
         for (int i = 0; i < vetor.length; i++) {
+            aux = 0;
             System.out.println("digite valores");
             vetor[i] = scan.nextDouble();
+            for (int j = 0; j < vetor.length; j++) {
+                if (vetor[i] == vetor[j]) {
+                    aux++;
+                    vetAuxiliar[i] = aux;
+                }
+            }
+        }
+    }
+
+    private void mostrar(int vetAuxiliar[], double vetor[], double vetFinal[]) {
+        for (int i = 0; i < vetor.length; i++) {
+            for (int j = 0; j < vetor.length; j++) {
+                if (vetor[i] == vetor[j]) {
+
+                }
+            }
+            System.out.println("Numero " + vetor[i] + " frequencia " + vetAuxiliar[i]);
         }
 
-
-        
     }
 
     public static void main(String[] args) {
