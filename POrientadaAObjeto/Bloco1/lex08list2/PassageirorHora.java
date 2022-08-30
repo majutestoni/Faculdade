b72d4b3e5689661d95a975ac183855f461262d5f
Maria Júlia Testoni <mjt@furb.br>
2022-08-30 09:05:01 -0300

    [change]ex08list2 pronto

--- POrientadaAObjeto/Bloco1/lex08list2/PassageirorHora.java
+++ POrientadaAObjeto/Bloco1/lex08list2/PassageirorHora.java
@@ -1,23 +1,62 @@
 package lex08list2;
 
-import java.util.Arrays;
-
 public class PassageirorHora {
-private int[][] matriz = new int[30][12];
+    private int[][][] matriz = new int[12][30][24];
 
     public void adiciona(int dia, int mes, int hora) {
-
+        matriz[mes - 1][dia - 1][hora]++;
     }
 
-    public int quantosPAssageiros() {
-        int a = 0;
+    public int quantosPAssageiros(int dia, int mes) {
+        int contador = 0;
+        for (int i = 0; i < matriz[mes][dia].length; i++) {
+            contador = contador + matriz[mes][dia][i];
+        }
+
+        return contador;
+    }
+
+    public int mesMenorFlux() {
+
+        int contador = 0;
+        int mesMenor = 100;
+
+        for (int i = 0; i < this.matriz.length; i++) {
+            for (int j = 0; j < this.matriz[i].length; j++) {
+                for (int j2 = 0; j2 < this.matriz[i][j].length; j2++) {
+                    contador = contador + this.matriz[i][j][j2];
+                }
+            }
+            if (contador < mesMenor) {
+                mesMenor = i;
+            }
+            contador = 0;
+        }
+
+        return mesMenor + 1;
+    }
+
+    public String picoTransporte() {
+        int contador = 0;
+        int mes = 0;
+        int dia = 0;
+        int hora = 0;
+
         for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz.length; j++) {
-                matriz[i][j] = 1;
+            for (int j = 0; j < matriz[i].length; j++) {
+                for (int j2 = 0; j2 < matriz[i][j].length; j2++) {
+                    if (matriz[i][j][j2] > contador) {
+                        contador = matriz[i][j][j2];
+                        hora = j2;
+                        dia = j;
+                        mes = i;
+                    }
+                }
             }
         }
 
-        System.out.println(Arrays.toString(matriz));
-        return a;
+        String teste = "mes: " + (mes + 1) + " dia: " + (dia + 1) + " e hora: " + hora;
+
+        return teste;
     }
 }
