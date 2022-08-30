a023e57793cf3db25901bd539d652ee942a001f4
Maria Júlia Testoni <mjt@furb.br>
2022-08-30 09:05:01 -0300

    [change]ex08list2 pronto

--- POrientadaAObjeto/Bloco1/lex08list2/Main.java
+++ POrientadaAObjeto/Bloco1/lex08list2/Main.java
@@ -2,9 +2,25 @@ package lex08list2;
 
 public class Main {
     public static void main(String[] args) {
+
+        PassageirorHora ph = new PassageirorHora();
+        ph.adiciona(24, 2, 5);
+        ph.adiciona(24, 2, 5);
+        ph.adiciona(24, 2, 5);
+        ph.adiciona(24, 2, 5);
+        ph.adiciona(30, 12, 5);
+        ph.adiciona(30, 12, 5);
+        ph.adiciona(30, 12, 5);
+        ph.adiciona(30, 12, 5);
+        ph.adiciona(30, 12, 5);
+        ph.adiciona(30, 12, 5);
+        ph.adiciona(30, 12, 5);
+        ph.adiciona(30, 12, 5);
+        ph.adiciona(30, 12, 5);
+        System.out.println(ph.quantosPAssageiros(24, 2));
+        System.out.println(ph.mesMenorFlux());
+        System.out.println(ph.picoTransporte());
         
-        PassageirorHora teste = new PassageirorHora();
-    teste.quantosPAssageiros();
     }
-    
+
 }
