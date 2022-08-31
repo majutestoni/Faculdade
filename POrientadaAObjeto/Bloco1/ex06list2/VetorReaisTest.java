6d27007cf6397ab94a30c1903d1c5258ecd6c4c9
Maria Júlia Testoni <mjt@furb.br>
2022-08-31 11:01:56 -0300

    [change] ex06list2

--- POrientadaAObjeto/Bloco1/ex06list2/VetorReaisTest.java
+++ POrientadaAObjeto/Bloco1/ex06list2/VetorReaisTest.java
@@ -1,41 +1,59 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
-class VetorReaisTest {
+class InteiroPositivoTest {
 
 	@Test
-	void testDivisao_vetoresTamanhosDiferentes() {
-		VetorReais v = new VetorReais(5);
-		VetorReais r = new VetorReais(3);
+	void test_fatorial_10() {
+		InteiroPositivo i = new InteiroPositivo();
 
-		v.setN(0, 2);
-		v.setN(1, 4);
-		v.setN(2, 5);
-		v.setN(3, 10);
-		v.setN(4, 10);
-		r.setN(0, 2);
-		r.setN(1, 4);
-		r.setN(2, 5);
-
-		assertNull(v.divisao(r));
+		assertEquals(3628800, i.fatorial(10), 0);
 	}
-	@Test
-	void testDivisao_vetoresTamanhosIguais() {
-		VetorReais v = new VetorReais(3);
-		VetorReais r = new VetorReais(3);
-		
-		v.setN(0, 2);
-		v.setN(1, -1);
-		v.setN(2, 3.5);
-		r.setN(0, 3);
-		r.setN(1, 2);
-		r.setN(2, 1);
-		
-		Double[] Vetor = {0.666666, -0.5, 3.5};
 
-		
-		assertArrayEquals(Vetor, v.divisao(r));
+	@Test
+	void test_fatorial_20() {
+		InteiroPositivo i = new InteiroPositivo();
+
+		i.setValor(20);
+		assertEquals(2432902008176640000L, i.fatorialValor(), 0);
+	}
+
+	@Test
+	void test_divisor_14() {
+		InteiroPositivo i = new InteiroPositivo();
+		i.setValor(14);
+
+		assertEquals(" 1, 2, 7, 14 e a quantidade de divisores é 4 ", i.divisores());
+	}
+
+	@Test
+	void test_divisor_18() {
+		InteiroPositivo i = new InteiroPositivo();
+		i.setValor(18);
+
+		assertEquals(" 1, 2, 3, 6, 9, 18 e a quantidade de divisores é 6 ", i.divisores());
+	}
+
+	@Test
+	void test_fibonacci_9() {
+		InteiroPositivo i = new InteiroPositivo();
+		i.setValor(9);
+		int[] Vetor = { 1, 1, 2, 3, 5, 8, 13, 21, 34 };
+
+		assertArrayEquals(Vetor, i.fibonacci());
+
+	}
+
+	@Test
+	void test_fibonacci_15() {
+		InteiroPositivo i = new InteiroPositivo();
+		i.setValor(15);
+		int[] Vetor = { 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610 };
+		assertArrayEquals(Vetor, i.fibonacci());
 	}
 
 }
