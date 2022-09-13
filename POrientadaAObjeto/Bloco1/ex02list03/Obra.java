798567f9d9b2a208d78606ad3ac3ef0484028966
Maju Testoni <majuliatestoni@gmail.com>
2022-09-13 17:58:42 -0300

    [change] adicionar ArrayList

--- POrientadaAObjeto/Bloco1/ex02list03/Obra.java
+++ POrientadaAObjeto/Bloco1/ex02list03/Obra.java
@@ -1,8 +1,12 @@
 package ex02list03;
+
+import java.util.ArrayList;
+
 public class Obra {
 	private String titulo;
 	private String autor;
 	private Parecer[] pareceres = new Parecer[3];
+	private ArrayList<Parecer> pareceristas = new ArrayList<Parecer>();
 	private int indice;
 
 	public String getTitulo() {
@@ -20,11 +24,23 @@ public class Obra {
 	public Parecer getParecer(int pos) {
 		return pareceres[pos];
 	}
+	public Parecer getParecerista() {
+		return pareceristas.toString;
+	}
+	public Parecer getPareceristaTudo(int pos) {
+		return pareceristas.get(pos);
+	}
 
 	public void addParecer(Parecer p) {
 		if (indice < pareceres.length) {
 			pareceres[indice] = p;
 			indice++;
+		}
+	}
+
+	public void addParecerista(Parecer p) {
+		if (pareceristas.size() < 4) {
+			pareceristas.add(p);
 		}
 	}
 
