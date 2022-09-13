c4d44fd07d90d676b5c0c408ea5c51c6888ad5fe
Maria Júlia Testoni <mjt@furb.br>
2022-09-13 09:04:12 -0300

    [add]ex02list03 parecer e obra

---
+++ POrientadaAObjeto/Bloco1/ex02list03/Obra.java
@@ -0,0 +1,35 @@
+package ex02list03;
+public class Obra {
+	private String titulo;
+	private String autor;
+	private Parecer[] pareceres = new Parecer[3];
+	private int indice;
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public Parecer getParecer(int pos) {
+		return pareceres[pos];
+	}
+
+	public void addParecer(Parecer p) {
+		if (indice < pareceres.length) {
+			pareceres[indice] = p;
+			indice++;
+		}
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+}
