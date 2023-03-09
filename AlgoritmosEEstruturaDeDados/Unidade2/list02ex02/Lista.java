416e55302814dbca5acce1d91a0f3084ec2e780e
maju testoni <majuliatestoni@gmail.com>
2023-03-09 16:34:57 -0300

    mexendo

---
+++ AlgoritmosEEstruturaDeDados/Unidade2/list02ex02/Lista.java
@@ -0,0 +1,29 @@
+package lista02ex01;
+
+
+
+public interface Lista {
+
+	void inserir(int valor);
+	
+	void inserir(int valor, int pos);
+	
+	int buscar(int valor);
+
+	boolean estaVazia();
+
+	void retirar(int valor);
+
+	String exibir();
+
+	Lista copiar();
+
+	Lista dividir();
+
+	int getTamanho();
+
+	void concatenar(Lista outraLista);
+
+	int pegar(int pos);
+
+}
