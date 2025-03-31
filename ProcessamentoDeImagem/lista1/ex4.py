d6eebbfe616d4292bc9ff211bf0c0d695d0b3039
majutestoni <majuliatestoni@gmail.com>
2025-03-31 18:32:22 -0300

    change

--- ProcessamentoDeImagem/lista1/ex4.py
+++ ProcessamentoDeImagem/lista1/ex4.py
@@ -1,19 +1,14 @@
 import cv2
 import matplotlib.pyplot as plt
 
-# Carregar imagem
 image = cv2.imread('colorida.jpg')
 
-# Converter para escala de cinza
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# Aplicar filtro mediano
 median_filtered = cv2.medianBlur(gray_image, 5)
 
-# Aplicar filtro de média (blur)
 blurred_image = cv2.blur(gray_image, (5, 5))
 
-# Visualizar as imagens
 plt.figure(figsize=(10, 7))
 plt.subplot(1, 4, 1)
 plt.title('Imagem Original')
@@ -35,9 +30,7 @@ plt.title('Filtro de Média')
 plt.imshow(blurred_image, cmap='gray')
 plt.axis('off')
 
-# Exibir resultados
 plt.show()
 
-# Salvar as imagens filtradas
 cv2.imwrite('imagem_median_filtered.jpg', median_filtered)
 cv2.imwrite('imagem_blurred.jpg', blurred_image)
--- ProcessamentoDeImagem/lista1/ex5.py
+++ ProcessamentoDeImagem/lista1/ex5.py
@@ -1,13 +1,10 @@
 import cv2
 import matplotlib.pyplot as plt
 
-# Carregar imagem colorida
 image = cv2.imread('colorida.jpg')
 
-# Converter para escala de cinza
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# Visualizar a imagem original e a imagem em escala de cinza
 plt.figure(figsize=(10, 7))
 
 plt.subplot(1, 2, 1)
@@ -22,16 +19,12 @@ plt.axis('off')
 
 plt.show()
 
-# Aplicar limiarização (thresholding)
-# O valor 127 é o limiar. Tudo acima de 127 se torna branco, abaixo se torna preto.
 _, threshold_image = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY)
 
-# Visualizar o resultado da limiarização
 plt.figure(figsize=(7, 7))
 plt.title('Imagem Limiarizada')
 plt.imshow(threshold_image, cmap='gray')
 plt.axis('off')
 plt.show()
 
-# Salvar a imagem limiarizada
 cv2.imwrite('imagem_limiarizada.jpg', threshold_image)
